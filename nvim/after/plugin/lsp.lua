e1804cd5befdffca2ca1a59c53a6bc0ea47feadf
Marton Szabo <szabo@xund.ai>
2024-02-23 11:06:54 +0100

    fix: small fixes

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -41,6 +41,7 @@ lsp.set_preferences({
 
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
+  client.server_capabilities.semanticTokensProvider = nil
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
